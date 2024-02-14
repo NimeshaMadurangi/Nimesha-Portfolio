23f1a32defa287c89556a2443c61ef36f5bdb547
IT21178986-Nimesha Madurangi <116791096+NimeshaMadurangi@users.noreply.github.com>
2024-02-14 22:08:32 +0530

    aa

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,12 +6,16 @@ function AboutMe() {
   return (
     <section id="about">
       <h1>About Me</h1>
+      <div className="about-content">
+        <p>Nimesha Madurangi</p>
+        <p>BSc. (Hons) in Information Technology</p>
         <div className="image-container2">
           <img src ="/female-developer-vector.jpg"  // Replace with the actual path to your image
             alt="Description of the image"
             className="profile-image"
           />
-        </div>
+          </div>
+      </div>
     </section>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,33 +5,27 @@ import Footer from './Footer';
 
 
 function Home() {
+
   return (
     <section id = "home">
     <div>
-      <h1>Welcome to My Portfolio</h1>
-        
-      <p>Explore my projects, skills, and more!</p>
-
-      <div className="home-content">
-      <h3>
-      Hey there! 👋 I’m Nimesha Madurangi, a third-year undergraduate student at the Sri Lanka Institute of Information Technology (SLIIT). Currently, I’m pursuing my BSc. (Hons) in Information Technology with a specialization in Information Technology! 🌟
-        </h3>
-
-      <div className="image-container">
-          <img
-            src="/programming_01.gif"
-            alt="Programming GIF"
-            className="programming-gif"
-          />
-        </div>
-        </div>
+      <div className="desc">
+      <h3>- Hello -</h3>
+      <h1>I'm Nimesha Madurangi</h1>
+      </div>
+      <br/>
+      <h3>Explore my projects, skills, and more!</h3>
       <br/>
         <button className="square-button"><a href="#about">About</a></button>
         <button className="square-button"><a href="#projects">Projects</a></button>
         <button className="square-button"><a href="#skills">Skills</a></button>
         <button className="square-button"><a href="#contact">Contact</a></button>
+        <br/>
+        <button className="square-button"><a href="#about">About</a></button>
+        <button className="square-button"><a href="#projects">Projects</a></button>
+        <button className="square-button"><a href="#skills">Skills</a></button>
+        <button className="square-button"><a href="#contact">Contact</a></button>
     </div>
-    <Footer/>
     </section>
   );
 }
