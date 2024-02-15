bef6e7ba699e9da99c7962247e78c6ec1a277e67
IT21178986-Nimesha Madurangi <116791096+NimeshaMadurangi@users.noreply.github.com>
2024-02-15 14:35:39 +0530

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Header from './Header';
-import Animation from './Animation';
 
 function Home() {
   return (
     <section id="home">
       <div className="content-container">
         <div className="desc">
+        <div className="animation-container">
+        <img
+          src="./wordanime.gif"
+          alt="Animation"
+          className="animation-gif"
+         />
+    </div>
+          <br/><br/><br/><br/> <br/><br/>
           <h3>- Hello -</h3>
           <h1>I'm Nimesha Madurangi</h1>
           <h3>Software Engineer</h3>
         </div>
-        <Animation />
-        <br />
-        <h4>Explore my projects, skills, and more!</h4>
-        <br />
-      </div>
+        </div>
     </section>
   );
 }
